1aad8db651d6ef0fdc2e403cc6c337575bd5405c
8721son <8721son@naver.com>
2023-03-16 12:17:18 +0900

    mypage

--- src/pages/My.js
+++ src/pages/My.js
@@ -1,7 +1,92 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import MyCard from "../components/common/MyCard";
+import CommonLayout from "../layouts/CommonLayout";
+import AuthStore from "../stores/AuthStore";
+import { customAxios } from "../utils/CustomAxios";
 
+// 사용자 정보
+// 내가 쓴 글
+// 좋아요 한 글
 const My = () => {
-  return <div></div>;
+  const authStore = AuthStore();
+
+  const [myPostList, setMyPostList] = useState([]);
+  const [likePostList, setLikePostList] = useState([]);
+
+  const getMyInfo = () => {
+    customAxios
+      .privateAxios({
+        method: "get",
+        url: "/v1/api/user/my",
+      })
+      .then((response) => {
+        if (response.status === 200) {
+          setMyPostList(response.data.content.myPostList);
+          setLikePostList(response.data.content.likePostList);
+        }
+      })
+      .catch(() => {})
+      .finally(() => {});
+  };
+
+  useEffect(() => {
+    if (authStore.loginUser === null) {
+      alert("로그인이 필요합니다");
+    }
+    getMyInfo();
+  }, []);
+
+  return (
+    <div>
+      <CommonLayout />
+      {authStore.loginUser ? (
+        <div>
+          <Container>
+            <Row>
+              <Col>
+                <div className='d-flex justify-content-center'>
+                  <img
+                    src={authStore.loginUser.profileImage}
+                    className='rounded-circle'
+                    style={{ width: "100px", height: "100px" }}
+                    alt='profile'
+                  />
+                </div>
+              </Col>
+              <Col>
+                <h2>{authStore.loginUser.id}</h2>
+                <p>{authStore.loginUser.simpleDesc}</p>
+                <Link to='/change-info'>내 정보 수정</Link>
+              </Col>
+            </Row>
+            <hr />
+          </Container>
+          <Container>
+            <Row className='row-cols-1 row-cols-md-2'>
+              <Col>
+                <h5 className='text-center'>내 글</h5>
+                <Row className='row-cols-1 card-group'>
+                  {myPostList.map((post) => (
+                    <MyCard post={post} />
+                  ))}
+                </Row>
+              </Col>
+              <Col>
+                <h5 className='text-center'>좋아요 한 글</h5>
+                <Row className='row-cols-1 card-group'>
+                  {likePostList.map((post) => (
+                    <MyCard post={post} />
+                  ))}
+                </Row>
+              </Col>
+            </Row>
+          </Container>
+        </div>
+      ) : null}
+    </div>
+  );
 };
 
 export default My;
--- src/pages/UpdatePost.js
+++ src/pages/UpdatePost.js
@@ -1,7 +1,176 @@
-import React from "react";
+import { Editor } from "@toast-ui/react-editor";
+import React, { useEffect, useRef, useState } from "react";
+import { Button, Col, Form, Image, Row } from "react-bootstrap";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import CommonLayout from "../layouts/CommonLayout";
+import ExitImg from "../assets/exit.svg";
+import AuthStore from "../stores/AuthStore";
+import { customAxios } from "../utils/CustomAxios";
 
 const UpdatePost = () => {
-  return <div></div>;
+  const authStore = AuthStore();
+  const navigate = useNavigate();
+
+  const [editorHeight, setEditorHeight] = useState(0);
+
+  const refs = useRef({
+    titleElement: null,
+    editorElement: null,
+  });
+
+  const params = useParams();
+  const postIdx = params.idx;
+
+  // 1. 게시물 인덱스 이용해서 서버랑통신해서 데이터 가져오고
+  // 에디터에 채워넣기
+
+  const getPost = () => {
+    // validation
+    if (isNaN(postIdx)) {
+      alert("잘못된 접근입니다.");
+      return;
+    }
+
+    customAxios
+      .privateAxios({
+        method: "get",
+        url: `/v1/api/post/${postIdx}`,
+      })
+      .then((response) => {
+        if (response.status === 200) {
+          refs.current.titleElement.value = response.data.content.title;
+          refs.current.editorElement
+            .getInstance()
+            .setMarkdown(response.data.content.content);
+        }
+      })
+      .catch(() => {})
+      .finally(() => {});
+  };
+
+  const validateFields = () => {
+    const { titleElement, editorElement } = refs.current;
+
+    if (titleElement.value === "") {
+      alert("제목을 입력해주세요.");
+      return false;
+    }
+
+    if (editorElement.getInstance().getMarkdown() === "") {
+      alert("내용을 입력해주세요.");
+      return false;
+    }
+    return true;
+  };
+
+  // 2. 수정
+  const update = () => {
+    // validation
+    // 제목이 빈값인지, 내용이 빈 값인지
+    if (!validateFields()) {
+      return;
+    }
+
+    const { titleElement, editorElement } = refs.current;
+
+    const title = titleElement.value;
+    const content = editorElement.getInstance().getMarkdown();
+
+    const markdownImageRegex = /\[.*\]\((.*)\)/gi; // 이미지
+    //html 태그, \n (html 문법)
+    const markdownRegex = /(\*|_|#|`|~|>|!|\[|\]|\(|\)|\{|\}|\||\\)/gi;
+
+    // 이미지 날리고, html날린 후 150자
+    const summary = content
+      .replace(markdownImageRegex, "")
+      .replace(markdownRegex, "")
+      .substring(0, 151);
+
+    const imageList = content.match(markdownImageRegex);
+    const thumbnailMarkdown = imageList != null ? imageList[0] : null;
+
+    const thumbnail = thumbnailMarkdown
+      ? thumbnailMarkdown.substring(
+          thumbnailMarkdown.indexOf("](") + 2,
+          thumbnailMarkdown.length - 1
+        )
+      : null;
+
+    const post = {
+      title: title,
+      content: content,
+      thumbnail: thumbnail,
+      summary: summary,
+    };
+    // axios
+    customAxios
+      .privateAxios({
+        method: "put",
+        url: `/v1/api/post/update/${postIdx}`,
+        data: post,
+      })
+      .then((response) => {
+        if (response.status === 200) {
+          alert("수정에 성공했습니다.");
+          navigate(`/post/${postIdx}`);
+        }
+      })
+      .catch(() => {})
+      .finally(() => {});
+  };
+
+  useEffect(() => {
+    setEditorHeight(`${window.innerHeight - 300}px`);
+    window.addEventListener("resize", () => {
+      setEditorHeight(`${window.innerHeight - 300}px`);
+    });
+  }, []);
+
+  useEffect(() => {
+    if (authStore.loginUser === null) {
+      alert("로그인이 필요한 기능입니다.");
+      navigate("/login");
+    }
+    getPost();
+  }, [authStore]);
+
+  return (
+    <div>
+      <CommonLayout />
+      <div style={{ minHeight: "100vh", overflowX: "hidden" }}>
+        <Row>
+          <Col>
+            <Form.Control
+              ref={(r) => (refs.current.titleElement = r)}
+              className='border-0 fs-1 mt-3'
+              type='text'
+              placeholder='제목을 입력해주세요.'
+            />
+          </Col>
+        </Row>
+        <Editor
+          ref={(r) => (refs.current.editorElement = r)}
+          previewStyle='vertical'
+          height={editorHeight}
+        />
+        <Row className='row fixed-bottom p-3 bg-white shadow-lg'>
+          <Col className='col-auto'>
+            {/* 나가기 */}
+            <Link to={-1}>
+              <Image src={ExitImg} />
+              <span>나가기</span>
+            </Link>
+          </Col>
+
+          <Col className='col-auto'>
+            <Button type='button' variant='outline-success' onClick={update}>
+              수정하기
+            </Button>
+          </Col>
+        </Row>
+      </div>
+    </div>
+  );
 };
 
 export default UpdatePost;
